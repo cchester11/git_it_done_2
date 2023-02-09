ae9cc8c7afaf604f46f4a613f3be6f5671b0e623
Charles <cchester1@angelo.edu>
2023-02-09 16:00:15 -0600

    successfully fetching issues for a single repo. comments written for writing the displayIssues function

--- public/assets/homepage.js
+++ public/assets/homepage.js
@@ -3,9 +3,9 @@ const submit_input_button = $('#submit_input_button')
 const search_value_span_el = $('#search_value_span_el')
 const search_results_div = $('#search_results_div')
 
-function displayResults (repos, user) {
+function displayResults(repos, user) {
       $(search_value_span_el).text(user)
-      if(repos.length === 0) {
+      if (repos.length === 0) {
             alert(`No results showing for ${user}`)
       }
 
@@ -20,19 +20,24 @@ function displayResults (repos, user) {
       })
 }
 
-// display single repo function
+// displayIssues function
+// takes a container parameter
+// takes in data parameter
+// maps data
+// creates dom container for data
+// appends dom container to container parameter
 
-function fetchRepos (user) {
+function fetchRepos(user) {
       fetch("https://api.github.com/users/" + user + "/repos", {
             method: 'get'
       })
             .then(results => {
-                  if(results.ok) {
+                  if (results.ok) {
                         results.json()
-                        .then(data => {
-                              displayResults(data, user)
-                              console.log(data)
-                        })
+                              .then(data => {
+                                    displayResults(data, user)
+                                    console.log(data)
+                              })
                   } else {
                         alert("Error: " + results.statusText)
                   }
@@ -42,17 +47,17 @@ function fetchRepos (user) {
             })
 }
 
-function fetchSingleRepo (repo) {
-      fetch("https://api.github.com/repos/" + repo + "/issues?direction=asc", {
-            method: 'get'
+function fetchSingleRepo(owner, repo) {
+      fetch("https://api.github.com/repos/"+owner+"/"+repo+"/issues?state=open", {
+            method: 'GET'
       })
             .then(results => {
-                  if(results.ok) {
+                  if (results.ok) {
                         results.json()
-                        .then(data => {
-                              displaySingleRepo(data)
-                              console.log(data)
-                        })
+                              .then(data => {
+                                    // displayIssues(data)
+                                    (data.length === 0) ? alert('There are no active issues associated with this repo') : console.log(data)
+                              })
                   } else {
                         alert('Error: ' + results.statusText)
                   }
@@ -62,10 +67,10 @@ function fetchSingleRepo (repo) {
             })
 }
 
-function inputSubmission (event) {
+function inputSubmission(event) {
       event.preventDefault()
       let user = username_input_value.val()
-      if(user) {
+      if (user) {
             fetchRepos(user)
             username_input_value.val('')
       } else {
@@ -75,13 +80,14 @@ function inputSubmission (event) {
 
 $(submit_input_button).on('click', inputSubmission)
 $(search_results_div).on('click', (event) => {
-      let repo_container = event.target.parentElement
+      let container = event.target.parentElement
       let repo = event.target.textContent
       let slash = repo.indexOf('/')
 
-      let new_repo_name = (slash !== 1) ? repo.substring(slash + 1) : alert('Please enter a valid repo name')
+      let owner = (slash !== -1) ? repo.substring(0, slash) : alert('Please enter a valid repo name')
+      let name = (slash !== -1) ? repo.substring(slash + 1) : alert('Please enter a valid repo name')
+      fetchSingleRepo(owner, name, container)
 
-      // call the fetch function for single repo with the spliced name
       // map through each issue (use display single repo function)
       // create dom elements that will contain each issue
       // append the list of issues to the clicked on div
