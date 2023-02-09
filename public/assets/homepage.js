cca770f357b40dba8cb146dddc75bf1f809ce928
Charles <cchester1@angelo.edu>
2023-02-08 21:58:27 -0600

    ..

--- public/assets/homepage.js
+++ public/assets/homepage.js
@@ -4,18 +4,13 @@ const submit_input_button = $('#submit_input_button')
 const search_value_span_el = $('#search_value_span_el')
 const search_results_div = $('#search_results_div')
 
-// display function: accepts parameters (repos, user)
-// do conditional check that makes sure repos return
-// loop over repos (try map) and create a container element for each
-// container includes a title (owners name/repo name)
-// statusEl (if repo has issues then it is active. if not it is inactive)
 function displayResults (repos, user) {
       $(search_value_span_el).text(user)
       if(repos.length === 0) {
             alert(`No results showing for ${user}`)
       }
 
-      repos.map((repo, i) => {
+      repos.map((repo) => {
             let container = $('<div>')
             let repoNameContainer = $('<h2>')
             let repoName = `${repo.full_name}`
@@ -26,10 +21,6 @@ function displayResults (repos, user) {
       })
 }
 
-// fetch function: fetches user repos from github api; takes parameter (user)
-// fetch the url using the parameter
-// retrieve the data and call the display function 
-// check for errors
 function fetchRepos (user) {
       fetch("https://api.github.com/users/" + user + "/repos", {
             method: 'get'
@@ -50,10 +41,6 @@ function fetchRepos (user) {
             })
 }
 
-// input function: accepts parameter (event)
-// captures value of input
-// fetches the fetch function, passes in input value
-// checks for errors
 function inputSubmission (event) {
       event.preventDefault()
       let user = username_input_value.val()
@@ -65,5 +52,9 @@ function inputSubmission (event) {
       }
 }
 
-// lastly, event listener that triggers input function
 $(submit_input_button).on('click', inputSubmission)
+$(search_results_div).on('click', (event) => {
+      let repo = event.target.value
+
+      console.log(repo)
+})
