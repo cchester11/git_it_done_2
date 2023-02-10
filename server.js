f1d22d1fc1ebbd42037f0621cd0634d19e2dc929
Charles <cchester1@angelo.edu>
2023-02-10 14:27:42 -0600

    server configured properly. serving homepage.handlebars successfully. needed to import the jquery cdn on the homepage layout itself. all works so far

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 // dependencies
 const express = require('express')
+const { engine } = require('express-handlebars')
 const path = require('path')
+
 const app = express()
+
+// set up handlebars as the view engine for our express middleware
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 // tells express to serve the whole folder so that the javascript will be read in the browser
 app.use(express.static(path.join(__dirname, 'public')))
 
 // route that renders the index.html on localhost:3000/
 app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, 'public', 'index.html'))
+      res.render('homepage')
 })
 
 // fire up the server
