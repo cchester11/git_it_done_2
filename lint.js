77c87c5fd6f0fe51fdd26445b929b22a3647c8b7
unknown <cchester1@angelo.edu>
2023-02-14 23:53:20 -0600

    conditional statement written for lint.js that runs according to the operating system being used to run the code

--- lint.js
+++ lint.js
@@ -3,17 +3,28 @@
 function lint_check() {
     // requiring the spawn method (an event method) from the 'child_process' module (similar to requiring the 'path' or 'fs' module)
     const { spawn } = require('child_process')
-    // first parameter is the listener?
-    const child = spawn('C://Users//Charles//scoop//apps//nodejs//current//node_modules//npm//bin//npm.cmd', ['run', 'lint'])
 
-    // It looks like this function will execute after the spawn method has run the lint script (on exit) and if an object returns with no errors, the call back will console.log('No errors') message below.
-    child.on('exit', (code) => {
-        if (code === 0) {
-            console.log('ESlint found no errors. Great Job!')
-        } else {
-            console.error('ESlint found ' + code +  ' errors with exit code.')
-        }
-    });
-}
+    if (process.platform === 'win32') {
+        const windows_child = spawn('C://Users//Charles//scoop//apps//nodejs//current//node_modules//npm//bin//npm.cmd', ['run', 'lint'])
+        // It looks like this function will execute after the spawn method has run the lint script (on exit) and if an object returns with no errors, the call back will console.log('No errors') message below.
+        windows_child.on('exit', (code) => {
+            if (code === 0) {
+                console.log('ESlint found no errors. Great Job!')
+            } else {
+                console.error('ESlint found ' + code + ' errors with exit code.')
+            }
+        });
+    } else if(process.platform === 'darwin') {
+        const mac_child = spawn('npm', ['run', 'lint'])
+
+        mac_child.on('exit', (code) => {
+            if(code === 0) {
+                console.log('ESlint found no errors. Great Job!')
+            } else {
+                console.error('ESlint found ' + code + ' errors with exit code.')
+            }
+        });
+    }
+};
 
 module.exports = lint_check;
